5e2d9ade019e8aac6f1d08e76c28137f0f79c53e
AranyaJha <aranyajha2000@gmail.com>
2020-12-10 19:36:09 +0530

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FeedbackReviewComponent } from './feedback-review/feedback-review.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
+import {FormsModule} from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FeedbackReviewComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatCardModule,
+    FormsModule,
+    HttpClientModule,
+    MatSnackBarModule,
+    MatButtonModule,
+    MatInputModule
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
